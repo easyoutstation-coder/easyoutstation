ca45eee40e4eae624b8d145c5db195ce7001cc0b
Parminder Singh <parmindersingh@Parminders-MacBook-Air.local>
2026-05-05 11:05:14 +0530

    fix vite config regex

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ const __dirname = import.meta.dirname
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    devServer({ entry: "api/boot.ts", exclude: [/^\\/(?!api\\/).*$/] }),
+    devServer({ entry: "api/boot.ts", exclude: [/^\/(?!api\/).*$/] }),
     react()
   ],
   server: {
